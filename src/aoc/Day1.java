9d9e1cbca6b7d36e275adf7b81f792350352abe1
Conrad THIOUNN <thiounn.conrad@gmail.com>
2018-12-19 11:59:45 +0100

    day1 completed

--- src/aoc/Day1.java
+++ src/aoc/Day1.java
@@ -3,7 +3,9 @@ package aoc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Day1 {
@@ -18,7 +20,20 @@ public class Day1 {
 
 	public static void part1And2(List<String> lines) {
 		System.out.println("" + lines.stream().collect(Collectors.summingDouble(Double::valueOf)).intValue());
-
-		// TODO part2 to be implemented
+		int sum = 0;
+		Map<Integer, Boolean> seen = new HashMap<>();
+		boolean stop = false;
+		while (!stop) {
+			for (String line : lines) {
+				sum += Integer.parseInt(line);
+				if (seen.get(sum) == null) {
+					seen.put(sum, true);
+				} else {
+					System.out.println(sum);
+					stop = true;
+					break;
+				}
+			}
+		}
 	}
 }
