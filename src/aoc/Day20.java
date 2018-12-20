286bd976adbfb26edc0faf07d7b2e3c2482ee940
Conrad THIOUNN <thiounn.conrad@gmail.com>
2018-12-20 12:18:08 +0100

    Day20 part2 temp (non-func = oom)

--- src/aoc/Day20.java
+++ src/aoc/Day20.java
@@ -15,15 +15,18 @@ public class Day20 {
 		System.out.println(unregexifyAll(input, false));
 		System.out.println(unregexifyAll(input, false).length() - 2);
 
+		System.out.println(unregexifyAllList(input).size());
 		System.out.println("runned time : " + (System.currentTimeMillis() - timeStart) + " ms");
 	}
 
 	private static String minify(String input) {
-		while (input.contains("EW") || input.contains("WE") || input.contains("NS") || input.contains("SN")) {
+		while (input.contains("(|)") || input.contains("EW") || input.contains("WE") || input.contains("NS")
+				|| input.contains("SN")) {
 			input = input.replace("EW", "");
 			input = input.replace("WE", "");
 			input = input.replace("NS", "");
 			input = input.replace("SN", "");
+			input = input.replace("(|)", "");
 		}
 		return input;
 	}
@@ -64,5 +67,58 @@ public class Day20 {
 			string = unregexify2(string, min);
 		}
 		return string;
+	}
+
+	private static List<String> unregexifyList(String string) {
+		List<String> allRegex = new ArrayList<>();
+		if (string.indexOf("(") >= 0) {
+			string = string.replace("(", "");
+			string = string.replace(")", "");
+			String[] listOptions = string.split("\\|");
+			for (String string2 : listOptions) {
+				allRegex.add(string2);
+			}
+		}
+		return allRegex;
+	}
+
+	private static List<String> unregexify2List(String string) {
+		List<String> allRegex = new ArrayList<>();
+		List<String> tempWork = new ArrayList<>();
+		int posBeginRegex = string.lastIndexOf("(");
+		int posEndRegex = (string.substring(posBeginRegex)).indexOf(")");
+		tempWork = unregexifyList(string.substring(posBeginRegex, posBeginRegex + posEndRegex + 1));
+		for (String string2 : tempWork) {
+			String result = string.substring(0, posBeginRegex) + string2
+					+ string.substring(posBeginRegex + posEndRegex + 1);
+			if (result.contains("(")) {
+				allRegex.add(result);
+			} else if (result.length() >= (1000 - 2)) {
+				allRegex.add(result);
+			} else {
+				System.out.println("remove toto2");
+			}
+		}
+		return allRegex;
+	}
+
+	private static List<String> unregexifyAllList(String string) {
+		List<String> allRegex = new ArrayList<>();
+		List<String> tempWork = new ArrayList<>();
+		tempWork.add(string);
+		while (tempWork.stream().filter(e -> e.contains("(")).count() != 0) {
+			String current = tempWork.remove(0);
+			if (current.contains("(")) {
+				tempWork.addAll(unregexify2List(current));
+			} else {
+				if (current.length() >= (1000 - 2)) {
+					tempWork.add(current);
+				} else {
+					System.out.println("remove toto");
+				}
+			}
+		}
+		allRegex.addAll(tempWork);
+		return allRegex;
 	}
 }
