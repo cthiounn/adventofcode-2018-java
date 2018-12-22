753bd48a9642c018ebcfb3e1440789ac9843c8e9
Conrad THIOUNN <thiounn.conrad@gmail.com>
2018-12-22 16:10:42 +0100

    Day 22 part2

--- src/aoc/Day22.java
+++ src/aoc/Day22.java
@@ -3,9 +3,12 @@ package aoc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.GrayFilter;
 
 public class Day22 {
 
@@ -13,6 +16,9 @@ public class Day22 {
 	static int yTarget = 0;
 	static int depth = 0;
 	static int modErosion = 20183;
+	static String keyTarget = "";
+
+	// static int equipement = 1; // 0 neither 1 torch 2 climbing gear
 
 	public static void main(String[] args) throws IOException {
 		long timeStart = System.currentTimeMillis();
@@ -20,12 +26,14 @@ public class Day22 {
 		depth = Integer.parseInt(input.get(0).split(" ")[1]);
 		xTarget = Integer.parseInt(input.get(1).split(" ")[1].split(",")[0]);
 		yTarget = Integer.parseInt(input.get(1).split(" ")[1].split(",")[1]);
+		keyTarget = xTarget + "!" + yTarget;
 		Map<String, Integer> grid = new HashMap<>();
 		initGrid(grid);
 
 		long totalRiskLevel = grid.entrySet().stream().filter(e -> inGrid(e.getKey()))
 				.mapToInt(e -> valueRisk(e.getValue())).sum();
 		System.out.println(totalRiskLevel);
+		shortestPathTo(grid);
 		System.out.println("runned time : " + (System.currentTimeMillis() - timeStart) + " ms");
 	}
 
@@ -43,8 +51,8 @@ public class Day22 {
 	private static void initGrid(Map<String, Integer> grid) {
 		int geologicIndex = 0;
 		int erosionLevel = 0;
-		for (int i = 0; i < depth; i++) {
-			for (int j = 0; j < depth; j++) {
+		for (int i = 0; i < 750; i++) {
+			for (int j = 0; j < 750; j++) {
 				if (i == j && j == 0) {
 					geologicIndex = 0;
 				} else if (i == xTarget && j == yTarget) {
@@ -62,4 +70,133 @@ public class Day22 {
 		}
 	}
 
+	private static void shortestPathTo(Map<String, Integer> grid) {
+		// from 0,0
+		Map<String, Integer> finalDest = new HashMap<>();
+		Map<String, Integer> queueGrid = new HashMap<>();
+		Map<String, Integer> newNodes = new HashMap<>();
+		Map<String, Integer> gridByMinutes = new HashMap<>();
+		queueGrid.put("0!0#1#0", 0);
+		gridByMinutes.put("0!0", 0);
+		int i = 0;
+		while (!queueGrid.isEmpty()) {
+			newNodes.clear();
+			for (Map.Entry<String, Integer> e : queueGrid.entrySet()) {
+				String node = e.getKey();
+				int x = Integer.parseInt(node.split("#")[0].split("!")[0]);
+				int y = Integer.parseInt(node.split("#")[0].split("!")[1]);
+				int tool = Integer.parseInt(node.split("#")[1]);
+				int dist = e.getValue();
+
+				int current = grid.get(x + "!" + y) % 3;
+				int bottom = grid.get((x + 1) + "!" + y) == null ? -1 : grid.get((x + 1) + "!" + y) % 3;
+				int top = grid.get((x - 1) + "!" + y) == null ? -1 : grid.get((x - 1) + "!" + y) % 3;
+				int left = grid.get(x + "!" + (y - 1)) == null ? -1 : grid.get(x + "!" + (y - 1)) % 3;
+				int right = grid.get(x + "!" + (y + 1)) == null ? -1 : grid.get(x + "!" + (y + 1)) % 3;
+
+				// r0 w1 n2
+				// 0 ->1,2 1->0,2 2-> 0,1
+				if (bottom != -1) {
+					int newDist = dist;
+					int newTool = intersect(current, bottom, tool);
+					String key = (x + 1) + "!" + y + "#" + newTool;
+					if ((current == bottom) || newTool == tool) { // same type
+						newDist += 1;
+					} else {
+						newDist += 8;
+					}
+					if (gridByMinutes.get(key) == null || gridByMinutes.get(key) > newDist) {
+						gridByMinutes.put(key, newDist);
+						if (newNodes.get(key) == null || newNodes.get(key) > newDist) {
+							newNodes.put(key, newDist);
+						}
+						if (key.startsWith(keyTarget)) {
+							if (finalDest.get(key) == null || finalDest.get(key) > newDist) {
+								finalDest.put(key, newDist);
+							}
+						}
+					}
+				}
+				if (top != -1) {
+					int newDist = dist;
+					int newTool = intersect(current, top, tool);
+					String key = (x - 1) + "!" + y + "#" + newTool;
+					if (current == top || newTool == tool) { // same type
+						newDist += 1;
+					} else {
+						newDist += 8;
+					}
+					if (gridByMinutes.get(key) == null || gridByMinutes.get(key) > newDist) {
+						gridByMinutes.put(key, newDist);
+						if (newNodes.get(key) == null || newNodes.get(key) > newDist) {
+							newNodes.put(key, newDist);
+						}
+						if (key.startsWith(keyTarget)) {
+							if (finalDest.get(key) == null || finalDest.get(key) > newDist) {
+								finalDest.put(key, newDist);
+							}
+						}
+					}
+				}
+				if (left != -1) {
+					int newDist = dist;
+					int newTool = intersect(current, left, tool);
+					String key = x + "!" + (y - 1) + "#" + newTool;
+					if (current == left || newTool == tool) { // same type
+						newDist += 1;
+					} else {
+						newDist += 8;
+					}
+					if (gridByMinutes.get(key) == null || gridByMinutes.get(key) > newDist) {
+						gridByMinutes.put(key, newDist);
+						if (newNodes.get(key) == null || newNodes.get(key) > newDist) {
+							newNodes.put(key, newDist);
+						}
+						if (key.startsWith(keyTarget)) {
+							if (finalDest.get(key) == null || finalDest.get(key) > newDist) {
+								finalDest.put(key, newDist);
+							}
+						}
+					}
+				}
+				if (right != -1) {
+					int newDist = dist;
+					int newTool = intersect(current, right, tool);
+					String key = x + "!" + (y + 1) + "#" + newTool;
+					if (current == right || newTool == tool) { // same type
+						newDist += 1;
+					} else {
+						newDist += 8;
+					}
+					if (gridByMinutes.get(key) == null || gridByMinutes.get(key) > newDist) {
+						gridByMinutes.put(key, newDist);
+						if (newNodes.get(key) == null || newNodes.get(key) > newDist) {
+							newNodes.put(key, newDist);
+						}
+						if (key.startsWith(keyTarget)) {
+							if (finalDest.get(key) == null || finalDest.get(key) > newDist) {
+								finalDest.put(key, newDist);
+							}
+						}
+					}
+				}
+			}
+			queueGrid.clear();
+			queueGrid.putAll(newNodes);
+		}
+		int minMinutes = finalDest.entrySet().stream().mapToInt(e -> e.getValue() + (e.getKey().contains("#1") ? 0 : 7))
+				.min().orElse(9999999);
+		System.out.println(minMinutes);
+	}
+
+	private static int intersect(int current, int bottom, int tool) {
+		if ((current == 1 && bottom == 2) || (current == 2 && bottom == 1)) {
+			return 0;
+		} else if ((current == 0 && bottom == 2) || (current == 2 && bottom == 0)) {
+			return 1;
+		} else if ((current == 1 && bottom == 0) || (current == 0 && bottom == 1)) {
+			return 2;
+		}
+		return tool;
+	}
 }
